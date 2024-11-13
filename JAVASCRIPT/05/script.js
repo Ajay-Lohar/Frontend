9f385adc732e844e30866c773409b0fdfd975a4c
ajaylohar <loharajay0019@gmail.com>
2024-11-13 11:58:29 +0530

    javascript

---
+++ JAVASCRIPT/05/script.js
@@ -0,0 +1,77 @@
+// console.log("3" + +3);//33  second value auto convert as string 
+   
+// //   console.log("3" ++3);//error
+
+//   console.log(3+ +"3" );//6  second value auto convert as number 
+
+//   console.log(message);
+
+//   _________________________________________________________________________________________________________
+
+//   // ternary operator
+
+//   let userage =2 ;
+
+//   const message = userage>18 ? " greterthan 18": "less than 18";
+
+//   console.log(message);
+
+//   _________________________________________________________________________________________________________
+
+
+
+
+
+
+//   if("ajay"){   //  string  truely value
+//   if("")    {   // empty string  falsy value
+//   if(+"0")  {   //  string  falsy value
+//   if(0)     {   // 0 , -0,+0  is falsy value  
+
+
+//     console.log("you can vote");
+//   }else{
+//     console.log("you can not vote");
+//   }
+
+
+
+//   _________________________________________________________________________________________________________
+
+// const day=2;
+
+// switch(day){ case 1 : console.log("Sunday"); break;
+//     case 2 : console.log("Monday"); break;
+
+// }
+
+
+//   _________________________________________________________________________________________________________
+
+
+//   functions
+
+
+//   function declaration
+// function greet(){
+//     console.log("good morning");
+// }
+
+
+
+// Call function
+// console.log(greet()); 
+
+// greet();
+
+//   _________________________________________________________Q_1________________________________________________
+// two number input and perform addition on them
+
+/*
+ function add(a,b){
+     console.log(a+b);
+     return a+b ;
+  }
+ add(1,3);
+ consol.log(add(1,3));
+*/
