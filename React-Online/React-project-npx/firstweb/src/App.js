82be3358cfb38544db151f194b29d11a168b7f10
ajaylohar <loharajay0019@gmail.com>
2024-12-17 18:24:49 +0530

    .

---
+++ React-Online/React-project-npx/firstweb/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="main">
+      <Header/>{/* Component */}
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
