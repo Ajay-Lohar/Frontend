07035d52cef508f69a8a870a1f2e78b970a9d4ee
ajaylohar <loharajay0019@gmail.com>
2024-12-18 16:17:36 +0530

    .

--- React-Online/React-project-npx/firstweb/src/App.js
+++ React-Online/React-project-npx/firstweb/src/App.js
@@ -1,28 +1,97 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row,Button } from 'react-bootstrap';
 import './App.css';
 import Footer from './Footer';
 import Header from './Header';
+import { blog } from './Data/blog';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faPhone, faSearch} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  let HeaderInfo={
+    email:'loharajay0019@gmail.com',
+    phone:'9876543210'
+  }
   return (
     <div className="main">
-      <Header/>{/* Component */}
-      <Container fluid>
+      <Header HeaderInfo={HeaderInfo} cname='ItShaala'>{/* Component */}
+        <h1>Cards  Blocks</h1>
+      <FontAwesomeIcon icon={faWhatsapp}/>
+      <FontAwesomeIcon icon={faPhone}/>
+      <FontAwesomeIcon icon={faSearch}/>
+      </Header>
+      {/* <Container fluid>
         <Container>
           <Row>
             <Col className='col-12 text-center py-4'>
-            Welcome Ajay
+            <h1>Welcome Ajay</h1>
             </Col>
           </Row>
-          <Row className='text-center'>
-            <Col lg='3' md='6'>Mi</Col>
-            <Col lg='3' md='6'>Ajay</Col>
-            <Col lg='3' md='6'>Gajanan</Col>
-            <Col lg='3' md='6'>Lohar</Col>
+          <Row >
+            <Col lg='3' md='6'>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Course 1</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+               </Card.Body>
+               </Card>
+             </Col>
+            <Col lg='3' md='6'>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Course 1</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+               </Card.Body>
+               </Card>
+             </Col>
+            <Col lg='3' md='6'>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Course 1</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+               </Card.Body>
+               </Card>
+             </Col>
+            <Col lg='3' md='6'>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Course 1</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+               </Card.Body>
+            </Card>
+             </Col>
+            
             
           </Row>
         </Container>
 
+      </Container> */}
+      <Container>
+        <Row>
+          {blog.map((v,i)=>{
+            return (
+              <ProductItems pitems={v} key={i}/>
+            )
+          }
+        
+        )}
+        </Row>
       </Container>
       <Footer/>
     </div>
@@ -30,3 +99,19 @@ function App() {
 }
 
 export default App;
+
+function ProductItems({pitems}){
+  return(
+    <div className="col-lg-3 mb-4">
+           <Card >
+              <Card.Body>
+                <Card.Title>{pitems.title}</Card.Title>
+                <Card.Text>
+                  {pitems.body}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+               </Card.Body>
+            </Card>
+    </div>
+  )
+}
--- React-Online/React-project-npx/firstweb/src/Header.jsx
+++ React-Online/React-project-npx/firstweb/src/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header({HeaderInfo,cname,children}) {//props names are optional 
+
+ // let{HeaderInfo,cname}=props;// thise props data save in HeaderInfo
+ //
+ console.log(cname)
   return (
-    <h1>Welcome To Header Section</h1>
+    //childern parameter is predifine in react props as children
+     <h1>{children}
+     </h1>
   )
 }
