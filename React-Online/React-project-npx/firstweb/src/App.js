959149ca97724ed903ed87141e5319d115a6ed0c
ajaylohar <loharajay0019@gmail.com>
2024-12-18 12:18:18 +0530

    .

--- React-Online/React-project-npx/firstweb/src/App.js
+++ React-Online/React-project-npx/firstweb/src/App.js
@@ -1,3 +1,4 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
 import Footer from './Footer';
 import Header from './Header';
@@ -6,6 +7,23 @@ function App() {
   return (
     <div className="main">
       <Header/>{/* Component */}
+      <Container fluid>
+        <Container>
+          <Row>
+            <Col className='col-12 text-center py-4'>
+            Welcome Ajay
+            </Col>
+          </Row>
+          <Row className='text-center'>
+            <Col lg='3' md='6'>Mi</Col>
+            <Col lg='3' md='6'>Ajay</Col>
+            <Col lg='3' md='6'>Gajanan</Col>
+            <Col lg='3' md='6'>Lohar</Col>
+            
+          </Row>
+        </Container>
+
+      </Container>
       <Footer/>
     </div>
   );
