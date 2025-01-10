0b7dda004ffb64003ccd295c2a6562f510b0aadd
ajaylohar <loharajay0019@gmail.com>
2025-01-10 21:35:32 +0530

    .

--- React-Offline/e-com-app/src/components/new-arrivals/NewArrivals.jsx
+++ React-Offline/e-com-app/src/components/new-arrivals/NewArrivals.jsx
@@ -1,23 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState ,useEffect} from 'react'
 import './NewArrivals.css'
 
 const NewArrivals = () => {
 
-    const [products,setProduct]= useState([]);
+    const [products,setProducts]= useState([]);
 
     
+    async function getProducts() {
+      
+  
+        const res = await fetch("https://dummyjson.com/products?limit=4");
+        const data = await res.json();
+        setProducts(data.products); 
+        
+     
+    }
+
+    useEffect(() => {
+      getProducts();
+    }, []);
+
   return (
    
-    <section class="cards-container">
-        <div class="card-heading">
+    <section className="cards-container">
+        <div className="card-heading">
             <h2>NEW ARRIVALS</h2>
         </div>
-        <div class="card-item flex center wrap">
-           
+        <div className="card-item flex center wrap">
+          {products.map((products)=>{
+            return(
+             <>
+              <div className="card">
+                <div className="item-img">
+                  <img src={products.thumbnail} />
+                  </div>
+                  <div className="item-text">
+                    <p>{products.title}</p>
+                    <p>
+                      <div className="rating">
+                       </div> 
+                       </p><div className="pricing">
+                        <p>{products.price}</p>
+                      </div>
+                    </div>
+              </div>
+             </>
+          )})
+
+          }
+          
         </div>
         
-        <div class="view-all-btn flex center">
-            <button class="show-all">View All</button>
+        <div className="view-all-btn flex center">
+            <button className="show-all">View All</button>
         </div>
     </section>
   )
