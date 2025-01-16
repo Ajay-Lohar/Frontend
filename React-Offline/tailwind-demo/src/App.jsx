da7819a68d1549743e68c9843d50e8cd2300058a
ajaylohar <loharajay0019@gmail.com>
2025-01-16 22:33:25 +0530

    .

---
+++ React-Offline/tailwind-demo/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+    <>
+    <h1 className='text-4xl text-red-500 te'>
+      Hello Ajay
+    </h1>
+    <h1 className=''>
+      Hello Ajay
+    </h1>
+    
+    </>
+  )
+}
+
+export default App
